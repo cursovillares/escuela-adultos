58a278b974205f2983d195ed1e6201f5bea0ba16
cursohuetor <cursovillares29@gmail.com>
2024-10-28 14:30:59 +0100

    nuevo cod iphone

--- js/script.js
+++ js/script.js
@@ -46,3 +46,45 @@ closeImageButton.addEventListener('click', () => {
     content.classList.remove('hidden');  // Mostrar de nuevo el contenido inicial (título y párrafo)
     playVideoButton.classList.remove('hidden');  // Volver a mostrar el botón de abrir video
 });
+
+
+////////////////////////////////////////////
+
+// Obtener los elementos
+/*
+const playVideoButton = document.getElementById('playVideoButton');
+const videoContainer = document.querySelector('.video-container');
+const video = document.getElementById('video');
+const content = document.querySelector('.content');
+const imageContainer = document.getElementById('imageContainer');
+const closeImageButton = document.getElementById('closeImageButton');
+*/
+// Función para detectar si el dispositivo es un iPhone
+function isIphone() {
+    return /iPhone/i.test(navigator.userAgent);
+}
+
+// Remover el atributo "controls" si el dispositivo es un iPhone
+if (isIphone()) {
+    video.removeAttribute('controls');
+}
+
+// Al hacer clic en el botón para reproducir el video
+playVideoButton.addEventListener('click', () => {
+    content.classList.add('hidden');  // Ocultar el título y el párrafo
+    videoContainer.classList.remove('hidden');  // Mostrar el video en pantalla completa
+    video.play();  // Reproducir el video
+});
+
+// Detectar cuando el video termina
+video.addEventListener('ended', () => {
+    videoContainer.classList.add('hidden');  // Ocultar el video cuando termina
+    imageContainer.classList.remove('hidden');  // Mostrar la imagen y el botón de cerrar
+});
+
+// Al hacer clic en el botón para cerrar la imagen
+closeImageButton.addEventListener('click', () => {
+    imageContainer.classList.add('hidden');  // Ocultar la imagen
+    content.classList.remove('hidden');  // Mostrar de nuevo el contenido inicial (título y párrafo)
+    playVideoButton.classList.remove('hidden');  // Volver a mostrar el botón de abrir video
+});
